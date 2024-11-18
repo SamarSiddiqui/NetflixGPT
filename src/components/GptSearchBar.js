7a7d3a8a21c5706489e4d50de5fd0946f07b25a4
SamarSiddiqui <samarsiddiqui3010@gmail.com>
2024-11-18 10:01:37 +0530

    changes

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -3,6 +3,7 @@ import lang from '../utils/language'
 import { useSelector } from 'react-redux'
 import { GoogleGenerativeAI } from '@google/generative-ai'
 import axios from 'axios';
+import { API_Options } from '../utils/constants';
 // const {GoogleGenerativeAI} = require('@google/generative-ai')
 // import { useRef } from 'react'
 
@@ -12,27 +13,35 @@ const GptSearchBar = () => {
   const langKey = useSelector((store)=>store.lang.lang)
   
   const inputValue = useRef(null)
-
-  const gptQuery = `Act as a movie recommendation system and suggest some movies for the query: ${inputValue?.current?.value}.Only give me the names of 5 movies, comma separated like the example result given ahead. Example Result: Koi Mil Gaya, Don, Sholay, Endgame, Whiplash`;
-
-   const handleGPTSearchClick = async () => {
   
-    console.log("Loading");
+  //SearchMOvie in TMDB
+  
+  const searchMovieTmdb = async(movie)=>{
+    const data = await fetch('https://api.themoviedb.org/3/search/movie?query='+ movie+'&include_adult=false&language=en-US&page=1', API_Options)
     
+    const json = await data.json()
+    
+    return json.results
+    
+  }
+  const handleGPTSearchClick = async () => {  
+    
+     const gptQuery = `Act as a movie recommendation system and suggest some movies for the query: ${inputValue?.current?.value}.Only give me the names of 5 movies, comma separated like the example result given ahead. Example Result: Koi Mil Gaya, Don, Sholay, Endgame, Whiplash`;
+    console.log("Loading");
     const response = await axios({
     url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyD2LJCkLBXJr6RdqjH9FKK_NHq761nhyh8",
     method: "post",
     data: {
     contents:[{parts:[{text:gptQuery}]}]
     }})
-
-    //  console.log(response?.data?.candidates[0]?.content?.parts[0]?.text);
-
-    const movieList = response?.data?.candidates[0]?.content?.parts[0].text.split(',')
-    
+    const movieList = response?.data?.candidates[0]?.content?.parts[0].text.split(',')    
     console.log(movieList);
+        
+    const promiseArray = movieList.map(movie=>searchMovieTmdb(movie))
+    const tmdbResults = await Promise.all(promiseArray)
+    // const json = await data.json()
+    console.log(tmdbResults);
      
-    
 }
     
   return (
